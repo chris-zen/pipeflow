ed4be82048d30b6752b903572859f9a6f9a6b95a
Christian Perez Llamas <christian.perez@schibsted.com>
2017-11-20 21:45:02 +0100

    Example1 fixes and improvements. Added Akka dependencies and mainClass. Added reference.conf Minor PipeFlow interface changes.

--- build.sbt
+++ build.sbt
@@ -3,15 +3,18 @@ import Keys._
 
 name := "pipeflow"
 
+lazy val akkaVersion = "2.3.16"
+
 lazy val defaultSettings = Defaults.coreDefaultSettings ++ Seq(
   scalaVersion := "2.10.6",
   crossScalaVersions := Seq("2.10.6", "2.11.11", "2.12.4"),
 
-  organization := "org.pipeflow",
+  organization := "com.github.chris-zen.pipeflow",
   version := "0.1",
 
   libraryDependencies ++= Seq(
     "com.typesafe" % "config" % "1.3.2",
+    "org.slf4j" % "slf4j-log4j12" % "1.7.25",
     "org.scalatest" %% "scalatest" % "3.0.4" % "test"
   )
 )
@@ -27,6 +30,8 @@ lazy val core = Project(id = "pipeflow-core", base = file("core"))
   .settings(defaultSettings ++ Seq(
     name := "PipeFlow Core",
     libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+      "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
       "com.assembla.scala-incubator" %% "graph-core" % "1.10.0"
     )
   ))
@@ -35,6 +40,7 @@ lazy val examples = Project(id = "pipeflow-examples", base = file("examples"))
   .settings(defaultSettings ++ Seq(
     name := "PipeFlow Examples",
     version := version.value,
+    mainClass := Some("pipeflow.examples.example1.Example1"),
     libraryDependencies ++= Seq(
     )
   ))
--- core/src/main/scala/pipeflow/system/PipeFlowSystem.scala
+++ core/src/main/scala/pipeflow/system/PipeFlowSystem.scala
@@ -2,36 +2,54 @@ package pipeflow.system
 
 import java.time.LocalDateTime
 
+import akka.actor.ActorSystem
 import com.typesafe.config.{Config, ConfigFactory}
+import org.slf4j.LoggerFactory
 import pipeflow.dsl.nodes.Node
 import pipeflow.system.iso8601.RepeatingInterval
 
+import scala.concurrent.duration.{Duration, FiniteDuration}
 import scala.util.{Failure, Success}
 
 
 object PipeFlowSystem {
   type NodeBuilder = (LocalDateTime) => Node
 
+  private val logger = LoggerFactory.getLogger(PipeFlowSystem.getClass.getName.split("[.$]").last)
+
   def apply(name: String): PipeFlowSystem = new PipeFlowSystem(name, ConfigFactory.load())
 }
 
-class PipeFlowSystem(name: String, config: Config) {
+class PipeFlowSystem(val name: String, val config: Config) {
 
-  import PipeFlowSystem.NodeBuilder
+  import PipeFlowSystem.{logger, NodeBuilder}
 
   case class Schedule(interval: RepeatingInterval, nodeBuilder: NodeBuilder)
 
-  var schedules = List.empty[Schedule]
+  val system = ActorSystem(name, config)
 
-  def schedule(iso8601Expression: String)(nodeBuilder: NodeBuilder): Unit =
+  def schedule(iso8601Expression: String)(nodeBuilder: NodeBuilder): Unit = {
     RepeatingInterval(iso8601Expression) match {
       case Success(repeatingInterval) => schedule(repeatingInterval)(nodeBuilder)
       case Failure(exception) => throw exception
     }
-
-  def schedule(repeatingInterval: RepeatingInterval)(nodeBuilder: NodeBuilder): Unit = {
-    schedules :+= Schedule(repeatingInterval, nodeBuilder)
   }
 
-  def run(): Unit = ???
+  def schedule(repeatingInterval: RepeatingInterval)(nodeBuilder: NodeBuilder): Unit = {
+    // TODO Create an scheduling actor
+  }
+
+  def run(node: Node): Unit = {
+    // TODO Materialize the node graph into actors
+  }
+
+  def awaitTermination(): Unit = {
+    logger.info("Awaiting PipeFlow termination ...")
+    system.awaitTermination()
+  }
+
+  def shutdown(): Unit = {
+    logger.info("Shutting down PipeFlow ...")
+    system.shutdown()
+  }
 }
